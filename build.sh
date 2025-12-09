b8133878ab9af450081090fa05e0081d2782f224
robot-ranger <ted@processrobotics.com>
2025-12-09 12:43:05 -0800

    build

--- build.sh
+++ build.sh
@@ -11,13 +11,11 @@ git clone --recurse-submodules https://github.com/FANUC-CORPORATION/fanuc_driver
 cd /fanuc-ws
 echo "Installing dependencies"
 rosdep update
-pwd
-ls -la src
-ls -la src/fanuc_driver
 rosdep install --ignore-src --from-paths src -y
 
 echo "Building FANUC libraries"
+# Source ROS setup before building
+source /opt/ros/$ROS_DISTRO/setup.bash
 colcon build --symlink-install --cmake-args -DBUILD_TESTING=1 -DBUILD_EXAMPLES=1
-
-echo "source /fanuc-ws/install/setup.bash" >> ~/.bashrc
 echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
+echo "source /fanuc-ws/install/setup.bash" >> ~/.bashrc
--- dockerfile
+++ dockerfile
@@ -4,5 +4,3 @@ COPY ./build.sh /fanuc-ws/build.sh
 RUN apt update \
     && chmod +x /fanuc-ws/build.sh \
     && /fanuc-ws/build.sh
-
-# RUN chmod +x /build.sh && /build.sh
